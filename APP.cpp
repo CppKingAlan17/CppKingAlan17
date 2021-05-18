69764734a4921757a77727f1aa68acd71f050b2b
CppKingAlan17 <84383187+CppKingAlan17@users.noreply.github.com>
2021-05-18 18:15:09 +0530

    This Is Stupid
    This IS the WORST THING I EVER MADE

---
+++ APP.cpp
@@ -0,0 +1,13 @@
+#include<windows.h>
+#include<iostream>
+using namespace std;
+int main(){
+  //The Only Super Man Can Notice The Diffrence 
+  while(1){
+  cout<<"Hello World"<<endl;
+  cout<<"Hello World"<<endl;
+  system("cls");
+  cout<<"Hello World"<<endl;
+  cout<<"WELL WELL"<<endl;
+  }
+}
